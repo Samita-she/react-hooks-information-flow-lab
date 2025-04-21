de0fc59b24682649ef2d0132d9b8402c635ea95c
Sheila Samita <samitasheila1@gmail.com>
2025-04-21 16:04:55 +0300

    hooks information flow lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,22 +3,25 @@ import React, { useState } from "react";
 function Item({ name, category }) {
   const [isInCart, setIsInCart] = useState(false);
 
-  function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
+  function handleClick() {
+    setIsInCart(!isInCart);
   }
 
+  const itemClass = isInCart ? "in-cart" : "";
+
   return (
-    <li className={isInCart ? "in-cart" : ""}>
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
       <button
         className={isInCart ? "remove" : "add"}
-        onClick={handleAddToCartClick}
+        onClick={handleClick}
       >
-        {isInCart ? "Remove From" : "Add to"} Cart
+        {isInCart ? "Remove From Cart" : "Add to Cart"}
       </button>
     </li>
   );
 }
 
 export default Item;
+
